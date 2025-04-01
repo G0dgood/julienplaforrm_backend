b18a1a9f0f079b8768acb6bfb73094f8ae2897ba
Mishael Dada <julienplaforrm@gmail.com>
2025-04-01 15:01:12 +0100

    fix

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,12 @@
-export default () => ({});
+export default ({ env }) => ({
+    upload: {
+      config: {
+        provider: "strapi-provider-upload-supabase",
+        providerOptions: {
+          apiUrl: env('SUPABASE_API_URL'),
+          apiKey: env('SUPABASE_API_KEY'),
+          bucket: env('SUPABASE_BUCKET'),
+        }
+      },
+    }, 
+  })
