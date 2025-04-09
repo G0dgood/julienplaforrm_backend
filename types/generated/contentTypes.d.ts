602269c7022d5959286e3462ccd6cdfe51c50c6d
Godwin Chinedu <chinedu.go@gmail.com>
2025-04-09 13:30:43 +0100

    updated client comment id

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -569,6 +569,10 @@ export interface ApiCampaignCampaign extends Struct.CollectionTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    user: Schema.Attribute.Relation<
+      'manyToOne',
+      'plugin::users-permissions.user'
+    >;
   };
 }
 
@@ -1230,6 +1234,7 @@ export interface PluginUsersPermissionsUser
   };
   attributes: {
     blocked: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    campaigns: Schema.Attribute.Relation<'oneToMany', 'api::campaign.campaign'>;
     client: Schema.Attribute.Relation<'oneToOne', 'api::client.client'>;
     comments: Schema.Attribute.Relation<'oneToMany', 'api::comment.comment'>;
     confirmationToken: Schema.Attribute.String & Schema.Attribute.Private;
