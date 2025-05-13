1afb10a020b5cd8351f95b7ce28b2c01b7dc79eb
Godwin Chinedu <chinedu.go@gmail.com>
2025-05-13 13:12:55 +0100

    update

---
+++ src/api/campaign-version/routes/campaign-version.ts
@@ -0,0 +1,7 @@
+/**
+ * campaign-version router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::campaign-version.campaign-version');
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -580,6 +580,36 @@ export interface ApiCampaignTrendCampaignTrend
   };
 }
 
+export interface ApiCampaignVersionCampaignVersion
+  extends Struct.CollectionTypeSchema {
+  collectionName: 'campaign_versions';
+  info: {
+    displayName: 'CampaignVersion';
+    pluralName: 'campaign-versions';
+    singularName: 'campaign-version';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    campaign_id: Schema.Attribute.String;
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::campaign-version.campaign-version'
+    > &
+      Schema.Attribute.Private;
+    publishedAt: Schema.Attribute.DateTime;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    version: Schema.Attribute.JSON;
+  };
+}
+
 export interface ApiCampaignCampaign extends Struct.CollectionTypeSchema {
   collectionName: 'campaigns';
   info: {
@@ -1445,6 +1475,7 @@ declare module '@strapi/strapi' {
       'api::buy-type.buy-type': ApiBuyTypeBuyType;
       'api::campaign-objective.campaign-objective': ApiCampaignObjectiveCampaignObjective;
       'api::campaign-trend.campaign-trend': ApiCampaignTrendCampaignTrend;
+      'api::campaign-version.campaign-version': ApiCampaignVersionCampaignVersion;
       'api::campaign.campaign': ApiCampaignCampaign;
       'api::client-signature-approval.client-signature-approval': ApiClientSignatureApprovalClientSignatureApproval;
       'api::client.client': ApiClientClient;
