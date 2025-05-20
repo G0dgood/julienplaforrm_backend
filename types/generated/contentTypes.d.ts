13705df5a7ab0e2042b49ff0ee403f4d4c1e4881
Mishael Dada <julienplaforrm@gmail.com>
2025-05-20 15:59:11 +0100

    fix

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -718,8 +718,8 @@ export interface ApiClientClient extends Struct.CollectionTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    user: Schema.Attribute.Relation<
-      'manyToOne',
+    users: Schema.Attribute.Relation<
+      'manyToMany',
       'plugin::users-permissions.user'
     >;
   };
@@ -1372,7 +1372,7 @@ export interface PluginUsersPermissionsUser
   attributes: {
     blocked: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
     campaigns: Schema.Attribute.Relation<'oneToMany', 'api::campaign.campaign'>;
-    clients: Schema.Attribute.Relation<'oneToMany', 'api::client.client'>;
+    clients: Schema.Attribute.Relation<'manyToMany', 'api::client.client'>;
     comments: Schema.Attribute.Relation<'oneToMany', 'api::comment.comment'>;
     confirmationToken: Schema.Attribute.String & Schema.Attribute.Private;
     confirmed: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
