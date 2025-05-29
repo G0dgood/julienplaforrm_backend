1d4ebf468d641662b46a38ae12912aab5e1af463
Mishael Dada <julienplaforrm@gmail.com>
2025-05-29 15:33:28 +0100

    fix

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -608,6 +608,7 @@ export interface ApiCampaignCampaign extends Struct.CollectionTypeSchema {
     >;
     progress_percent: Schema.Attribute.Integer;
     publishedAt: Schema.Attribute.DateTime;
+    selected_metrics: Schema.Attribute.JSON;
     table_headers: Schema.Attribute.JSON;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
