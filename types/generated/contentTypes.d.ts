2d89798fc2f2fc229b7d14a76f97e60696c887cb
Godwin Chinedu <chinedu.go@gmail.com>
2025-05-30 08:57:22 +0100

    make update

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -611,10 +611,7 @@ export interface ApiCampaignCampaign extends Struct.CollectionTypeSchema {
     >;
     progress_percent: Schema.Attribute.Integer;
     publishedAt: Schema.Attribute.DateTime;
-<<<<<<< HEAD
-=======
     selected_metrics: Schema.Attribute.JSON;
->>>>>>> cbdd26303b1138f1de37c4a7a5b20babf425d4a1
     table_headers: Schema.Attribute.JSON;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
